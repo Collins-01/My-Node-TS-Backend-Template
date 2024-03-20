0747560a1d88675a4b850be786294c153bc822a2
Collins <oriakhicolls01@gmail.com>
2024-03-20 05:59:24 +0100

    Added datbase setup

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,26 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  database: '',
+  password: '',
+  logging:true,
+  
+  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
 export default class DB {
-    public connect = async ()=>[]
+
+
+
+// Option 3: Passing parameters separately (other dialects)
+
+    public connect = async ()=>{
+        try {
+            await sequelize.authenticate();
+            console.log('Connection has been established successfully.');
+          } catch (error) {
+            console.error('Unable to connect to the database:', error);
+          }
+    }
 }
--- src/models/user_model.ts
+++ src/models/user_model.ts
@@ -1,1 +1,22 @@
-class User {}
+import { Model,DataTypes,  } from "sequelize";
+import  { sequelize } from "../database";
+class User  extends Model{
+
+}
+
+User.init({
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    lastName: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    }
+  }, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User' // We need to choose the model name
+  });
